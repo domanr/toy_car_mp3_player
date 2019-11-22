d8181df9a7b3c157a212242919ba9ef0f41187a0
Rajmund Doman <domanr@gmail.com>
2019-11-22 16:24:14 +0100

    Baudrate is set to 9600

--- Serial.cpp
+++ Serial.cpp
@@ -17,14 +17,14 @@ void Serial::init(void)
     //UCBRx = 8, UCBRFx = 0, UCBRSx = 0xD6, UCOS16 = 0
     EUSCI_A_UART_initParam param = {0};
     param.selectClockSource = EUSCI_A_UART_CLOCKSOURCE_SMCLK;
-    param.clockPrescalar = 8;
-    param.firstModReg = 0;
-    param.secondModReg = 0xD6;
+    param.clockPrescalar = 6;
+    param.firstModReg = 8;
+    param.secondModReg = 0x20;
     param.parity = EUSCI_A_UART_NO_PARITY;
     param.msborLsbFirst = EUSCI_A_UART_LSB_FIRST;
     param.numberofStopBits = EUSCI_A_UART_ONE_STOP_BIT;
     param.uartMode = EUSCI_A_UART_MODE;
-    param.overSampling = EUSCI_A_UART_LOW_FREQUENCY_BAUDRATE_GENERATION;
+    param.overSampling = EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION;
 
     if (STATUS_FAIL == EUSCI_A_UART_init(EUSCI_A0_BASE, &param)) {
         return;
